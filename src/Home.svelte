9770ee763dc1035128d35459c144ad1bf985c621
Michael Speed Elder <michael.speed.elder@gmail.com>
2022-12-04 14:42:27 -0600

    make striped sections responsive

--- src/Home.svelte
+++ src/Home.svelte
@@ -3,9 +3,11 @@
   import Faq from "./Faq.svelte";
 </script>
 
-<section>
-  <h2>Games List</h2>
-  Giving away some games, eh? <Link to="/list">Show me what you got.</Link>
+<section class="stripe">
+  <div class="stripe-inner">
+    <h2>Games List</h2>
+    Giving away some games, eh? <Link to="/list">Show me what you got.</Link>
+  </div>
 </section>
 
 <section>
@@ -26,94 +28,68 @@
   </ul>
 
   <p>
-    I've attached a spreadsheet so you can see what the options are. Message me back about any
-    you're interested in, and I'll update the sheet anonymously.
-  </p>
-
-  <p>
-    Have a look around, let me know if I can help with anything, and I hope that our next <abbr
-      title="you know it's &quot;brunch and board games&quot; don't even pretend">B&BG</abbr
-    > get together is in the very near future.
+    Have a look around, let me know if I can help with anything, and I hope that our next brunch and
+    board games is in the very near future.
   </p>
 </section>
 
-<section>
-  <h2>Games List</h2>
-  <Link to="/list">Just show me the games.</Link>
+<section class="stripe">
+  <div class="stripe-inner">
+    <h2>Games List</h2>
+    <Link to="/list">Just show me the games.</Link>
+  </div>
 </section>
 
 <section>
   <Faq />
 </section>
 
-<section>
-  <h2>Games List</h2>
-  Holy cow, <Link to="/list">I just want to see the games.</Link>
+<section class="stripe">
+  <div class="stripe-inner">
+    <h2>Games List</h2>
+    Holy cow, <Link to="/list">I just want to see the games.</Link>
+  </div>
 </section>
 
 <style lang="scss">
-  @import "./global.scss";
+  @import "./styles/global.scss";
 
   section {
-    // box-sizing: content-box;
     --max-section-width: 62ch;
     margin: 3em auto 0;
-    padding: 0 2ch;
-    width: var(--max-section-width);
+    padding: 0;
+    max-width: var(--max-section-width);
 
-    &:nth-child(2n) {
+    &.stripe {
       background-color: $global-bg-color-alt;
-      // box-sizing: content-box;
-      padding: 2ch 0;
-      position: relative;
-      // overflow: hidden;
+      margin-left: -2ch;
+      margin-right: -2ch;
+      padding: 2ch;
+      max-width: calc(4ch + 100%);
 
-      &::before,
-      &::after {
-        content: "";
-        background-color: var(--red);
-        width: calc((var(--page-content-width, 0)));
-        // width: 100px;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        font-size: 0;
-      }
+      .stripe-inner {
+        margin: 0 auto;
+        max-width: var(--max-section-width);
 
-      &::before {
-        transform: translateX(-100%);
-      }
-
-      &::after {
-        left: 100%;
-      }
-
-      h2 {
-        margin-top: 0;
+        h2 {
+          margin-top: 0;
+        }
       }
     }
   }
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: $breakpoint-lg) {
     section {
-      $max-section-width: 768px;
-      --max-section-width: $max-section-width;
+      $max-section-width: $global-max-width-lg;
+      --max-section-width: #{$max-section-width};
       padding: 0;
-      width: $max-section-width;
+      max-width: $max-section-width;
 
-      // &:nth-child(2n) {
-      //   // margin-left: -2ch;
-      //   // margin-right: -2ch;
-      //   // padding: 2ch unquote("max((") (2ch, 100vw - #{$max-section-width} unquote(")") / 2)
-      //   //   unquote(")");
-      //   // margin-left: 0;
-      //   // margin-right: 0;
-      //   // padding: 2ch unquote("max(2ch, (100vw - #{$max-section-width}) / 2)");
-      // }
+      &.stripe {
+        margin-left: initial;
+        margin-right: initial;
+        padding: 2ch 0;
+      }
     }
   }
-
-  /* section + section {
-    margin-top: 3em;
-  } */
 </style>
