b47d76ddd883b6e50e875fedbce035f861691b9a
Michael Speed Elder <michael.speed.elder@gmail.com>
2022-12-17 14:15:54 -0600

    make faqdetail component

--- src/Faq.svelte
+++ src/Faq.svelte
@@ -1,125 +1,76 @@
+<script>
+  import FaqDetail from "./FaqDetail.svelte";
+</script>
+
 <h2>Q&A</h2>
 
-<details>
-  <summary>I see a game I gave you on the list! What the hell?!</summary>
-  <div class="details-inner">
-    <p>
-      It's probably a duplicate. I am so fortunate to have people in my life who know that
-      unwrapping a new board game is a joy I cherish above all others, and as I don't keep a public
-      list of games somewhere, duplicates were inevitable. But fear not! I'm only giving away <em
-        >someone else's</em
-      > copy, not yours, the one that I love and treasure.
-    </p>
-    <p>
-      I've also had the pleasure to play many of these extensively at a different time in my life,
-      and I'm hoping they can bring other gamers as much joy as they've brought me over the years.
-    </p>
-  </div>
-</details>
+<FaqDetail question="I see a game I gave you on the list! What the hell?!">
+  <p>
+    It's probably a duplicate. I am so fortunate to have people in my life who know that unwrapping
+    a new board game is a joy I cherish above all others, and as I don't keep a public list of games
+    somewhere, duplicates were inevitable. But fear not! I'm only giving away <em>someone else's</em
+    > copy, not yours, the one that I love and treasure.
+  </p>
+  <p>
+    I've also had the pleasure to play many of these extensively at a different time in my life, and
+    I'm hoping they can bring other gamers as much joy as they've brought me over the years.
+  </p>
+</FaqDetail>
 
-<details>
-  <summary>Why are you downsizing now?</summary>
-  <div class="details-inner">
-    <p>
-      Do you see how many games are on this list? The list of games I'm giving away? This isn't half
-      of the games I have. I live in a warehouse. And if I don't do it soon, I'm going to be living
-      in a warehouse <em>alone</em>.
-    </p>
-  </div>
-</details>
+<FaqDetail question="Why are you downsizing now?">
+  <p>
+    Do you see how many games are on this list? The list of games I'm giving away? This isn't half
+    of the games I have. I live in a warehouse. And if I don't do it soon, I'm going to be living in
+    a warehouse <em>alone</em>.
+  </p>
+</FaqDetail>
 
-<details>
-  <summary>Why do you have so many unopened games?</summary>
-  <div class="details-inner">
-    <p>
-      That's a rude thing to ask an American; everyone knows the important thing is Having More
-      Stuff&trade;, rather than enjoying what you already have.
-    </p>
-    <p>
-      Just kidding, the issue was mostly optimism. A genuine hope that adults with busy lives would
-      have endless time to try new games, and a willing imagination that fed me image after image of
-      a happy brunch & board games for each shrink-wrapped box.
-    </p>
-    <p>
-      There may also have been one or two purchases appended to reach free shipping thresholds. Hey,
-      I already admitted I was American.
-    </p>
-  </div>
-</details>
+<FaqDetail question="Why do you have so many unopened games?">
+  <p>
+    That's a rude thing to ask an American; everyone knows the important thing is Having More
+    Stuff&trade;, rather than enjoying what you already have.
+  </p>
+  <p>
+    Just kidding, the issue was mostly optimism. A genuine hope that adults with busy lives would
+    have endless time to try new games, and a willing imagination that fed me image after image of a
+    happy brunch & board games for each shrink-wrapped box.
+  </p>
+  <p>
+    There may also have been one or two purchases appended to reach free shipping thresholds. Hey, I
+    already admitted I was American.
+  </p>
+</FaqDetail>
 
-<details>
-  <summary>Are the opened games in good condition?</summary>
-  <div class="details-inner">
-    <p>
-      I am pretty protective about my games, but the ones that have been played more may show a
-      little wear and tear. Nothing that would make the game unplayable or unsightly. Plus, it's
-      hard to beat the low price of free.
-    </p>
-  </div>
-</details>
+<FaqDetail question="Are the opened games in good condition?">
+  <p>
+    I am pretty protective about my games, and I would feel comfortable saying most of them are
+    "like new." Some of the ones that have been played more may show a little wear, though nothing
+    that would make the game unplayable or unsightly. Plus, it's hard to beat the low price of free.
+  </p>
+</FaqDetail>
 
-<details>
-  <summary>I don't live in Austin. Will you ship it to me?</summary>
-  <div class="details-inner">
-    <p>If you don't mind Venmoing me the shipping, I'll gladly put it in a box with an address.</p>
-  </div>
-</details>
+<FaqDetail question="I don't live in Austin. Will you ship it to me?">
+  <p>If you don't mind Venmoing me the shipping, I'll gladly put it in a box with an address.</p>
+</FaqDetail>
 
-<details>
-  <summary>Seems like it might not arrive before &lt;NEXT_BIG_HOLIDAY&gt;</summary>
-  <div class="details-inner">
-    <p>
-      That's hardly the only occasion to give someone a gift—the results can be even better when
-      they're not expecting it. But if you need a solution for right now, you could always wrap an
-      index card or a Post-It "coupon," and let the recipient know that Santa's elves are not beyond
-      the reach of global shipping delays.
-    </p>
-  </div>
-</details>
+<FaqDetail question="Seems like it might not arrive before &lt;NEXT_BIG_HOLIDAY&gt;">
+  <p>
+    That's hardly the only occasion to give someone a gift—the results can be even better when
+    they're not expecting it. But if you need a solution for right now, you could always wrap an
+    index card or a Post-It "coupon," and let the recipient know that Santa's elves are not beyond
+    the reach of global shipping delays.
+  </p>
+</FaqDetail>
 
-<details>
-  <summary
-    >I've got some games I'd like to offer/donate as well. Can I add them to your list?</summary
-  >
-  <div class="details-inner">
-    <p>Sure! Message me.</p>
-  </div>
-</details>
+<FaqDetail
+  question="I've got some games I'd like to offer/donate as well. Can I add them to your list?"
+>
+  <p>Sure! Message me.</p>
+</FaqDetail>
 
-<details>
-  <summary>Can I forward this list to other people?</summary>
-  <div class="details-inner">
-    <p>
-      Yes, but I am very inured to messages from outside my contact list, so I can't promise I won't
-      miss something if I don't know it's coming.
-    </p>
-  </div>
-</details>
-
-<style lang="scss">
-  @import "./styles/colors.scss";
-
-  summary {
-    cursor: pointer;
-    transition: color 100ms linear;
-
-    [open] &,
-    &:hover {
-      color: orange;
-    }
-  }
-
-  details {
-    margin-top: 0.5em;
-
-    &[open] {
-      margin-bottom: 1.5em;
-    }
-
-    .details-inner {
-      background-color: $global-bg-color-alt;
-      margin-top: 0.5rem;
-      padding: 0.1px 2rem;
-    }
-  }
-</style>
+<FaqDetail question="Can I forward this list to other people?">
+  <p>
+    Yes, but I am very inured to messages from outside my contact list, so I can't promise I won't
+    miss something if I don't know it's coming.
+  </p>
+</FaqDetail>
---
+++ src/FaqDetail.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let question: string;
+</script>
+
+<details>
+  <summary>{question}</summary>
+  <div class="details-inner">
+    <slot />
+  </div>
+</details>
+
+<style lang="scss">
+  @import "./styles/colors.scss";
+
+  details {
+    margin-top: 0.5em;
+
+    &[open] {
+      margin-bottom: 1.5em;
+    }
+
+    .details-inner {
+      background-color: $global-bg-color-dark;
+      margin-top: 0.5rem;
+      padding: 0.1px 2rem;
+    }
+  }
+
+  summary {
+    cursor: pointer;
+    transition: color 100ms linear;
+
+    [open] &,
+    &:hover {
+      color: orange;
+    }
+  }
+</style>
